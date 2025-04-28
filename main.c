eee56cd270ed0e6fe4dabdd7f2b472dd1dfc9512
Rek <altrekdv8@gmail.com>
2025-04-28 12:46:11 +0000

    add

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+using namespace std;
+
+int add(int x, int y) {
+    return x+y;
+}
+
 int main() {
     int a, b, c;
     a = 2; b = 4; c = 5;
-    
+    int sum = add(a, b);
+
+    cout<<sum<<"\n";
 
     return 0;
 }
