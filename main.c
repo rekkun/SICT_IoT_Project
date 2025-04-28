f2da15ba514fc14aea2badb1f1d1f655024cc59b
Phongcnttta01 <phongqv11a1@gmail.com>
2025-04-28 20:36:20 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "HaMinhNghia.c"
-
+#include "nguyendangphong.c"
 int main() {
-    int a, b, c;
+    float a, b, c;
     a = 2; b = 4; c = 5;
     printf(add(a, b));
-
+    printf(nhanSoThuc(a,b));
     return 0;
 }
